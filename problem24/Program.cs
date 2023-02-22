cc884b94157b517c366fce876a22cd867132570e
ezz2003 <ezz2003@yandex.ru>
2023-02-22 11:38:18 +1000

    solving problem24 and problem26

---
+++ problem24/Program.cs
@@ -0,0 +1,25 @@
+﻿// 24. Написать программу, которая принимает на вход число (А) и выдаёт сумму всех чисел от 1 до А.
+// 
+// 
+
+
+int InputInt(string message)
+{
+  Console.Write(message + ": ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumConsecutiveNumbers(int num)
+{
+  int sum = 0;
+  while (num > 0)
+  {
+    checked { sum += num; }   // Checked - для обработчика ошибок
+    num--;
+  }
+  // for (int i = 1; i <= num; i++) sum += i;
+  return sum;
+}
+
+
+Console.WriteLine(SumConsecutiveNumbers(InputInt("Введите натуральное число")));
---
+++ problem26/Program.cs
@@ -0,0 +1,20 @@
+﻿// 26. Напишите программу, которая принимает на вход число, и выдаёт количество цифр в числе
+
+int InputInt(string message)
+{
+  Console.Write(message + ": ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountingNumbers(int num)
+{
+  int count = 1;
+  while (true)
+  {
+    num = num / 10;
+    if (num == 0) return count;
+    count++;
+  }
+}
+
+Console.WriteLine($"В введённом числе {CountingNumbers(InputInt("Введите число"))} цифр");
