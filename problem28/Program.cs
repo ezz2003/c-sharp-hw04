61ebc759f16e5bd4a188919e1c04c0d192ad884d
ezz2003 <ezz2003@yandex.ru>
2023-02-22 20:34:38 +1000

    solved the problem28

---
+++ problem28/Program.cs
@@ -0,0 +1,25 @@
+﻿// 24. Написать программу, которая принимает на вход число (N) и выдаёт произведение всех чисел от 1 до N.
+// 
+// 
+
+
+int InputInt(string message)
+{
+  Console.Write(message + ": ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetFactorial(int num)
+{
+  int f = 1;
+  while (num > 0)
+  {
+    checked { f *= num; }   // Checked - для обработчика ошибок
+    num--;
+  }
+  // for (int i = 1; i <= num; i++) f *= i;
+  return f;
+}
+
+
+Console.WriteLine($"Факториал введённого числа = {GetFactorial(InputInt("Введите натуральное число"))}");
