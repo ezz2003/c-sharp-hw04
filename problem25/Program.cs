92223064db8486965a701cb6edc2a57a449ed63d
ezz2003 <ezz2003@yandex.ru>
2023-02-23 17:46:53 +1000

    soling the problem25

---
+++ problem25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 
+// 3, 5 -> 243 (3⁵)
+// 
+// 2, 4-> 16
+
+int InputInt(string message)
+{
+  Console.Write(message + ": ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int CustomPow(int num, int degree)
+{
+  int res = 1;
+  for (int i = 0; i < degree; i++) res *= num;
+  return res;
+}
+
+int a = InputInt("Введите число - основание");
+int b = InputInt("Введите степень числа");
+Console.WriteLine($"{a}, {b} -> {(CustomPow(a, b))}");
+
