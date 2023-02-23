cb61b6bd666e13f92c200197b68b28cf86500e25
ezz2003 <ezz2003@yandex.ru>
2023-02-23 19:58:44 +1000

    solving the problem29

---
+++ problem29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+
+int[] GenerateArray()
+{
+  int[] array = new int[] { 1, 2, 5, 7, 19, 6, 1, 33 };
+  return array;
+}
+
+string StrForPrintArray(int[] arr)
+{
+  string strPrn = String.Empty;
+  for (int i = 0; i < arr.Length - 1; i++) strPrn += $"{arr[i]}, ";
+  return strPrn += $"{arr.Last()}";
+}
+
+Console.WriteLine($"[{StrForPrintArray(GenerateArray())}]");
